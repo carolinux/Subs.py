1af959779c8aa19ea9877ddb68213ac7df752bbf
carolinux <carolinegr@gmail.com>
2011-08-12 23:46:24 +0300

    Edited subs.py via GitHub

--- subs.py
+++ subs.py
@@ -1,34 +1,31 @@
 import datetime
 
-#i wonder if abstract methods are possibul, im a c++ person!!
-# is self.blah really necessary? :S
-# null /xception handling...
-
-class SubtitleFile:
+class subtitle_file:
 
     def __init__(self,fileptr):
         self.file=fileptr
-    
-class SrtFile(SubtitleFile):
-    
-    separator="-->"
-    def isTimeLine(self,line):
+
+class srt_file(subtitle_file):
+
+    separator = "-->"
+
+    def is_time_line(self,line):
         array = line.split()
         if(len(array)==3 and array[1]==self.separator):
-           return True
+            return True
         return False
-    
-    def addTime(self,line,add_time,writePos):
+
+    def add_time(self,line,add_time,writePos):
         array=line.split()
-        start_time= self.parseTimeFromString(array[0]);
+        start_time= self.parse_time_from_string(array[0]);
         start_time = start_time + datetime.timedelta(milliseconds= add_time)
-        end_time= self.parseTimeFromString(array[2]);
+        end_time= self.parse_time_from_string(array[2]);
         end_time = end_time + datetime.timedelta(milliseconds= add_time)
-        self.formatTime(end_time)
+        self.format_time(end_time)
         self.file.seek(writePos,0)
-        self.file.write(self.formatTime(start_time)+" "+self.separator+" "+self.formatTime(end_time)+"\n")
-        
-    def parseTimeFromString(self,timestring): #format: hh:mm:ss,mmm
+        self.file.write(self.format_time(start_time)+" "+self.separator+" "+self.format_time(end_time)+"\n")
+
+    def parse_time_from_string(self,timestring): #format: hh:mm:ss,mmm
 
         array1= timestring.split(":") #what if wrong format?
         hours = int(array1[0])
@@ -38,26 +35,7 @@ class SrtFile(SubtitleFile):
         milliseconds= int(array2[1])
         return datetime.datetime(1,1,1,hours,minutes,seconds, milliseconds * 1000)
 
-    def formatTime(self,dt_object):
+    def format_time(self,dt_object):
         return str(dt_object.hour).zfill(2) +":"+str(dt_object.minute).zfill(2)+":"+str(dt_object.second).zfill(2)+","+str(int(dt_object.microsecond/1000)).zfill(3)
      
-#read user input
-filepath = raw_input('Enter the path of the srt file: ')
-add_time = raw_input('Enter time to add in milliseconds (negative values for substraction: ')
-#catch excepshuns
-f = open(filepath, 'r+')
-sf = SrtFile(f) #can add support for other subtitle files
-add_time = int(add_time) #milliseconds 
-#parse the file line by line
-start_of_write= f.tell()
-line=f.readline()
 
-while  (line!=""):
-    if sf.isTimeLine(line):#only parse lines with time values in them
-       sf.addTime(line,add_time,start_of_write)
-    start_of_write= f.tell()
-    line=f.readline()
-    
-f.close()
-print "Finished converting your file"
-exit()
