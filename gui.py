3fb62336ef420a5c684e9836fa1726574fcae416
carolinux <carolinegr@gmail.com>
2011-08-12 23:40:37 +0300

    Edited gui.py to be python 2.7 + added some comments for exception handling in teh future //minor edit

--- gui.py
+++ gui.py
@@ -90,19 +90,19 @@ class Subfixer:
             line=self.f.readline()
             
             while  (line!=""):
-                #print(line)
+                #print line 
                 if self.sf.is_time_line(line):#only parse lines with time values in them
                     self.sf.add_time(line,self.add_time,start_of_write)
                 start_of_write= self.f.tell()
                 line=self.f.readline()
 
             self.f.close()
-            print("Finished converting your file")
+            print "Finished converting your file"
 
         def apply_clicked(self, btn):
                 #self.browser.open(self.text.get_text())
-                self.add_time = int(self.text.get_text())
-                print(self.add_time)
+                self.add_time = int(self.text.get_text()) #IS THIS AN INT OR NOT? 
+                print self.add_time
                 self.process_file()
 
         def choose_clicked(self, btn):
@@ -113,8 +113,8 @@ class Subfixer:
                 chooser_dialog.set_default_response(1)
 
                 if chooser_dialog.run() == 1:
-                    print(chooser_dialog.get_filename())
-                    filepath = chooser_dialog.get_filename()
+                    print chooser_dialog.get_filename()
+                    filepath = chooser_dialog.get_filename() #IS THIS A SRT FILE? (EXTENSION CHECK)
                     self.f = open(filepath, 'r+')
                     self.sf = srt_file(self.f)
                     chooser_dialog.destroy()
